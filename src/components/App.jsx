b2912a4d81cc8cb589d38711f7365c11fafa4dcd
d-sofiika <soniadrozd2709@gmail.com>
2024-11-26 01:20:22 +0200

    added form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import HomePage from '../pages/HomePage/HomePage';
 import SignInPage from '../pages/SignInPage/SignInPage';
 import SignUpPage from '../pages/SignUpPage/SignUpPage';
 import TrackerPage from '../pages/TrackerPage/TrackerPage';
+import SettingModal from '../pages/SettingModal/SettingModal';
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
             <Route path="/signin" element={<SignInPage />} />
             <Route path="/signup" element={<SignUpPage />} />
             <Route path="/tracker" element={<TrackerPage />} />
+            <Route path="/setting" element={<SettingModal />} />
           </Routes>
         </Suspense>
       </SharedLayout>
--- src/pages/SettingModal/SettingModal.jsx
+++ src/pages/SettingModal/SettingModal.jsx
@@ -1,24 +1,103 @@
-import React, { useState } from "react";
-import Modal from "../../shared/Modal/Modal.jsx";
-import XClose from "../../shared/Icons/xClose.svg"
-
+import React, { useId, useState } from 'react';
+/* import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { updateAvatar } from '../../redux/user/operations.js';
+*/
+import Modal from '../../shared/Modal/Modal.jsx';
+import css from './SettingModal.module.css';
+import eye from '../../shared/Icons/eye.svg';
 const SettingModal = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const {nameId, emailId, timeId, weightId,amountId} = useId();
 
+  /*
+  
+  const dispatch = useDispatch();
+  const { user, isLoading } = useSelector(state => state.auth); // отримуємо статус та користувача з Redu
+  const [avatarFile, setAvatarFile] = useState(null);
+
+  const handleFileChange = e => {
+    const file = e.target.files[0];
+    if (file) {
+      setAvatarFile(file);
+    }
+  }; 
+  */
+  
+  
+  /* 
+  const handleSubmit = async () => {
+    if (avatarFile) {
+      const formData = new FormData();
+      formData.append('avatar', avatarFile);
+
+      try {
+        await dispatch(updateAvatar(formData)).unwrap();
+        alert('Avatar updated successfully!');
+      } catch (error) {
+        alert('Failed to update avatar');
+      }
+    }
+  };
+  
+  const handleSubmitForm = evt => {
+    evt.preventDefault();
+    console.log(evt);
+  };
+  */
   return (
     <div>
       <h1>Test Page</h1>
       <button onClick={openModal}>Open Modal</button>
       <Modal isOpen={isModalOpen} onClose={closeModal}>
-       
-        <h2>Setting</h2>
-        <div>
-          <button>Upload a photo</button>
+        <div className={css.formCover}>
+          <h2>Setting</h2>
+          <form >
+            <div>
+              <p>Upload your avatar</p>
+              <p>No avatar set</p>
+
+              <input type="file" accept="image/*"  />
+
+              <button>Update Avatar</button>
+            </div>
+            <h3>Your gender identity</h3>
+            <label>
+              <input type="radio" name="gender" value="woman" />
+              Woman
+            </label>
+            <label>
+              <input type="radio" name="gender" value="man" />
+             Man
+            </label>
+
+            <label htmlFor={nameId}><h3>Your name</h3></label>
+            <input type="text" id={nameId} />
+            <label htmlFor={emailId}><h3>Email</h3></label>
+            <input type="email" id={emailId} />
+            <h3>My daily norma</h3>
+            <p>For woman:</p>
+            <p>V=(M*0,03) + (T*0,4)</p>
+            <p>For man:</p>
+            <p>V=(M*0,04) + (T*0,6)</p>
+            <p>* V is the volume of the water norm in liters per day, M is your body weight, T is the time of active sports, or another type of activity commensurate in terms of loads (in the absence of these, you must set 0)</p>
+            <div>
+              <img src={eye} alt="Mark" className={css.markIcon} />
+              <p>Active time in hours</p>
+            </div>
+            <label htmlFor={weightId}>Your weight in kilograms:</label>
+            <input type="number" id={weightId} />
+            <label htmlFor={timeId}>The time of active participation in sports:</label>
+            <input type="number" id={timeId} />
+            <p>The required amount of water in liters per day:</p>
+            <p>1.8 L</p>
+            <label htmlFor={amountId}><h3>Write down how much water you will drink:</h3></label>
+            <input type="number" id={amountId} />
+            <button type="submit">Save</button>
+          </form>
         </div>
-        
       </Modal>
     </div>
   );
