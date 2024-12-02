a1c1e0d072a95c4e3549184b1a0e75566d35a4c5
d-sofiika <soniadrozd2709@gmail.com>
2024-12-03 00:42:48 +0200

    done adaptation

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -7,13 +7,17 @@ import { AiOutlineUser } from 'react-icons/ai';
 import Modal from '../../shared/Modal/Modal.jsx';
 import css from './SettingModal.module.css';
 import Icon from '../../shared/Icons/Icon.jsx';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUser } from '../../redux/user/operations.js';
 
 const SettingModal = () => {
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [waterIntake, setWaterIntake] = useState(1.5);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const user = useSelector(state => state.auth.user);
+  const dispatch = useDispatch();
 
   const validationSchema = Yup.object({
     name: Yup.string()
@@ -49,8 +53,8 @@ const SettingModal = () => {
   } = useForm({
     resolver: yupResolver(validationSchema),
     defaultValues: {
-      name: '',
-      email: '',
+      name: user?.name || '',
+      email: user?.email || '',
       weight: '0',
       activeHours: '0',
       waterIntake: '1.5',
@@ -59,8 +63,10 @@ const SettingModal = () => {
   });
 
   const onSubmit = async data => {
+    setIsLoading(true);
+
     const formData = new FormData();
-    if (data.avatar) {
+    if (data.avatar?.length > 0) {
       formData.append('avatar', data.avatar[0]);
     }
     formData.append('name', data.name);
@@ -71,14 +77,15 @@ const SettingModal = () => {
     formData.append('gender', data.gender);
 
     try {
-      const response = await fetch('/your-api-endpoint', {
-        method: 'POST',
-        body: formData,
-      });
-      const result = await response.json();
+      await dispatch(updateUser(formData)).unwrap();
       console.log('Form submitted successfully:', result);
+      alert('Дані успішно збережено!');
+      setIsModalOpen(false);
     } catch (error) {
       console.error('Error submitting form:', error);
+      alert('Сталася помилка під час відправки даних.');
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -100,8 +107,6 @@ const SettingModal = () => {
     event.preventDefault();
     fileInputRef.current.click();
   };
-
-
 
   // Generation of identifiers
   const nameId = useId();
@@ -174,136 +179,150 @@ const SettingModal = () => {
               </label>
             </div>
           </div>
-          <div className={`${css.partCover} ${css.box}`}>
-            <div>
-              <label htmlFor={nameId} className={css.secondTitle}>
-                Your name
-              </label>
-              <input
-                type="text"
-                id={nameId}
-                className={`${css.input} ${errors.name ? css.errorInput : ''}`}
-                {...register('name')}
-              />
-              {errors.name && (
-                <span className={css.error}>{errors.name.message}</span>
-              )}
-            </div>
-            <div>
-              <label htmlFor={emailId} className={css.secondTitle}>
-                Email
-              </label>
-              <input
-                type="email"
-                id={emailId}
-                className={`${css.input} ${errors.email ? css.errorInput : ''}`}
-                {...register('email')}
-              />
-              {errors.email && (
-                <span className={css.error}>{errors.email.message}</span>
-              )}
-            </div>
-          </div>
-          <div className={css.partCover}>
-            <div className={css.formulaCover}>
-              <div>
-                <p className={css.descr}>For woman:</p>
-                <p className={css.greenDscr}>V=(M*0,03) + (T*0,4)</p>
+          <div className={css.coverForLaptop}>
+            <div className={css.miniCoverLaptop}>
+              <div className={`${css.partCover} ${css.box}`}>
+                <div>
+                  <label htmlFor={nameId} className={css.secondTitle}>
+                    Your name
+                  </label>
+                  <input
+                    type="text"
+                    id={nameId}
+                    className={`${css.input} ${errors.name ? css.errorInput : ''}`}
+                    {...register('name')}
+                  />
+                  {errors.name && (
+                    <span className={css.error}>{errors.name.message}</span>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor={emailId} className={css.secondTitle}>
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id={emailId}
+                    className={`${css.input} ${errors.email ? css.errorInput : ''}`}
+                    {...register('email')}
+                  />
+                  {errors.email && (
+                    <span className={css.error}>{errors.email.message}</span>
+                  )}
+                </div>
               </div>
-              <div>
-                <p className={css.descr}>For man:</p>
-                <p className={css.greenDscr}>V=(M*0,04) + (T*0,6)</p>
+              <div className={`${css.partCover} ${css.withoutMargin}`}>
+                <h3 className={`${css.secondTitle} ${css.updateDailyUp}`}>
+                  My daily norma
+                </h3>
+                <div className={css.formulaCover}>
+                  <div>
+                    <p className={css.descr}>For woman:</p>
+                    <p className={css.greenDscr}>V=(M*0,03) + (T*0,4)</p>
+                  </div>
+                  <div>
+                    <p className={css.descr}>For man:</p>
+                    <p className={css.greenDscr}>V=(M*0,04) + (T*0,6)</p>
+                  </div>
+                </div>
+                <p className={css.formulaDescr}>
+                  <span className={css.formulaStar}>*</span> V is the volume of
+                  the water norm in liters per day, M is your body weight, T is
+                  the time of active sports, or another type of activity
+                  commensurate in terms of loads (in the absence of these, you
+                  must set 0)
+                </p>
+                <h3 className={`${css.secondTitle} ${css.updateDailyDown}`}>
+                  My daily norma
+                </h3>
+                <div className={css.coverMarkIcon}>
+                  <Icon id="icon-alert" width={18} height={18} />
+                  <p className={`${css.descr} ${css.actives}`}>
+                    Active time in hours
+                  </p>
+                </div>
               </div>
             </div>
-            <p className={css.formulaDescr}>
-              <span className={css.formulaStar}>*</span> V is the volume of the
-              water norm in liters per day, M is your body weight, T is the time
-              of active sports, or another type of activity commensurate in
-              terms of loads (in the absence of these, you must set 0)
-            </p>
-            <h3 className={css.secondTitle}>My daily norma</h3>
-            <div className={css.coverMarkIcon}>
-              <Icon id="icon-alert" width={18} height={18} />
-              <p className={`${css.descr} ${css.actives}`}>
-                Active time in hours
-              </p>
-            </div>
-          </div>
-          <div className={`${css.partCover} ${css.box}`}>
-            <div>
-              <label htmlFor={weightId} className={css.descr}>
-                Your weight in kilograms:
+            <div className={css.miniCoverLaptop}>
+              <div className={`${css.partCover} ${css.box}`}>
+                <div>
+                  <label htmlFor={weightId} className={css.descr}>
+                    Your weight in kilograms:
+                  </label>
+                  <Controller
+                    name="weight"
+                    control={control}
+                    render={({ field }) => (
+                      <input
+                        type="number"
+                        id={weightId}
+                        className={`${css.input} ${errors.weight ? css.errorInput : ''}`}
+                        {...field}
+                      />
+                    )}
+                  />
+                  {errors.weight && (
+                    <span className={css.error}>{errors.weight.message}</span>
+                  )}
+                </div>
+                <div>
+                  <label htmlFor={timeId} className={css.descr}>
+                    The time of active participation in sports:
+                  </label>
+                  <Controller
+                    name="activeHours"
+                    control={control}
+                    render={({ field }) => (
+                      <input
+                        type="number"
+                        id={timeId}
+                        className={`${css.input} ${errors.activeHours ? css.errorInput : ''}`}
+                        {...field}
+                      />
+                    )}
+                  />
+                  {errors.activeHours && (
+                    <span className={css.error}>
+                      {errors.activeHours.message}
+                    </span>
+                  )}
+                </div>
+              </div>
+              <div className={css.limitAmount}>
+                <p className={`${css.descr} ${css.descrTablet}`}>
+                  The required amount of water in liters per day:
+                </p>
+                <p className={css.greenDscr}>{waterIntake} L</p>
+              </div>
+              <label htmlFor={amountId} className={css.secondTitle}>
+                Write down how much water you will drink:
               </label>
               <Controller
-                name="weight"
+                name="waterIntake"
                 control={control}
                 render={({ field }) => (
                   <input
                     type="number"
-                    id={weightId}
-                    className={`${css.input} ${errors.weight ? css.errorInput : ''}`}
+                    id={amountId}
+                    step="0.1"
+                    className={`${css.input} ${errors.waterIntake ? css.errorInput : ''}`}
                     {...field}
+                    value={waterIntake}
+                    onChange={e => {
+                      const value = parseFloat(e.target.value) || 0;
+                      setWaterIntake(value);
+                      field.onChange(e);
+                    }}
                   />
                 )}
               />
-              {errors.weight && (
-                <span className={css.error}>{errors.weight.message}</span>
-              )}
-            </div>
-            <div>
-              <label htmlFor={timeId} className={css.descr}>
-                The time of active participation in sports:
-              </label>
-              <Controller
-                name="activeHours"
-                control={control}
-                render={({ field }) => (
-                  <input
-                    type="number"
-                    id={timeId}
-                    className={`${css.input} ${errors.activeHours ? css.errorInput : ''}`}
-                    {...field}
-                  />
-                )}
-              />
-              {errors.activeHours && (
-                <span className={css.error}>{errors.activeHours.message}</span>
+              {errors.waterIntake && (
+                <span className={css.error}>{errors.waterIntake.message}</span>
               )}
             </div>
           </div>
-          <div className={css.limitAmount}>
-            <p className={css.descr}>
-              The required amount of water in liters per day:
-            </p>
-            <p className={css.greenDscr}>{waterIntake} L</p>
-          </div>
-          <label htmlFor={amountId} className={css.secondTitle}>
-            Write down how much water you will drink:
-          </label>
-          <Controller
-            name="waterIntake"
-            control={control}
-            render={({ field }) => (
-              <input
-                type="number"
-                id={amountId}
-                step="0.1"
-                className={`${css.input} ${errors.waterIntake ? css.errorInput : ''}`}
-                {...field}
-                value={waterIntake}
-                onChange={e => {
-                  const value = parseFloat(e.target.value) || 0;
-                  setWaterIntake(value);
-                  field.onChange(e);
-                }}
-              />
-            )}
-          />
-          {errors.waterIntake && (
-            <span className={css.error}>{errors.waterIntake.message}</span>
-          )}
           <button type="submit" className={css.btnSubmit}>
-            Save
+            {isLoading ? 'Saving...' : 'Save'}
           </button>
         </form>
       </Modal>
