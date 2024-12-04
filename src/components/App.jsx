e3ff290f734dac780825cd5423132c14aaf5e955
d-sofiika <soniadrozd2709@gmail.com>
2024-12-05 00:46:24 +0200

    added calculation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -84,7 +84,6 @@ const App = () => {
                   <PrivateRoute component={<TrackerPage />} redirectTo="/" />
                 }
               />
-              <Route path="/setting" element={<SettingModal />} />
               <Route path="*" element={<NotFoundPage />} />
             </Routes>
           </Suspense>
