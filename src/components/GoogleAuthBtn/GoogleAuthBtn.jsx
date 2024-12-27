ac662a9c8f9a6b5740b84ba2016c020c62606653
Dumitru <dima50066@gmail.com>
2024-12-27 11:05:05 +0100

    rework google btn

--- src/components/GoogleAuthBtn/GoogleAuthBtn.jsx
+++ src/components/GoogleAuthBtn/GoogleAuthBtn.jsx
@@ -1,24 +1,35 @@
 import css from './GoogleAuthBtn.module.css';
 import google from '../../shared/Icons/google.svg';
-import { motion } from "framer-motion";
-import { useTranslation } from "react-i18next";
+import { motion } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-hot-toast';
 
 function GoogleAuthBtn() {
   const { t } = useTranslation();
-    return (
-<div className={css.wrap}> 
- <p className={css.text}>{t('or')}</p>
- <motion.a
+
+  const handleClick = e => {
+    e.preventDefault();
+    toast.error(t('Coming soon!'), {
+      duration: 3000,
+      position: 'top-center',
+    });
+  };
+
+  return (
+    <div className={css.wrap}>
+      <p className={css.text}>{t('or')}</p>
+      <motion.a
         whileHover={{ scale: 1.05 }}
         whileTap={{ scale: 1.01 }}
-        href="https://five-force-fronted.vercel.app/users/get-oauth-url"
-        className={css.btn}
+        href="#"
+        onClick={handleClick}
+        className={`${css.btn} ${css.disabled}`}
       >
-        <img src={google} className={css.icon} />
-        <p className={css.textGoogle}>{t("Sign in with Google")}</p>
+        <img src={google} className={css.icon} alt="Google icon" />
+        <p className={css.textGoogle}>{t('Sign in with Google')}</p>
       </motion.a>
-</div>
-)
-};
+    </div>
+  );
+}
 
 export default GoogleAuthBtn;
--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://fiveforce-aquatrack.onrender.com';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
